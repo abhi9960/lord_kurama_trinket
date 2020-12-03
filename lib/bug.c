d10c2788badc930e988ee351c81955071d7907bd
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2020-12-04 05:12:04 +0700

    lib: Import realmeme code changes

--- lib/bug.c
+++ lib/bug.c
@@ -47,6 +47,10 @@
 #include <linux/bug.h>
 #include <linux/sched.h>
 #include <linux/rculist.h>
+#ifdef ODM_WT_EDIT
+// Hui.Wang@ODM_WT.BSP.Kernel.Stability.1941873, 2019/05/31, Add for display boot reason
+#include <wt_sys/wt_boot_reason.h>
+#endif
 
 extern struct bug_entry __start___bug_table[], __stop___bug_table[];
 
@@ -190,11 +194,25 @@ enum bug_trap_type report_bug(unsigned long bugaddr, struct pt_regs *regs)
 
 	printk(KERN_DEFAULT "------------[ cut here ]------------\n");
 
-	if (file)
+	if (file) {
 		pr_crit("kernel BUG at %s:%u!\n", file, line);
-	else
+#ifdef ODM_WT_EDIT
+// Hui.Wang@ODM_WT.BSP.Kernel.Stability.1941873, 2019/05/31, Add for display boot reason
+#ifdef CONFIG_WT_BOOT_REASON
+		save_panic_key_log("kernel BUG at %s:%u!\n", file, line);
+#endif
+#endif
+	} else {
 		pr_crit("Kernel BUG at %p [verbose debug info unavailable]\n",
 			(void *)bugaddr);
+#ifdef ODM_WT_EDIT
+// Hui.Wang@ODM_WT.BSP.Kernel.Stability.1941873, 2019/05/31, Add for display boot reason
+#ifdef CONFIG_WT_BOOT_REASON
+		save_panic_key_log("Kernel BUG at %p \n", (void *)bugaddr);
+#endif
+#endif
+/* bug 407890, wanghui2.wt, 2018/12/12, add save_panic_key_log, end */
+	}
 
 	return BUG_TRAP_TYPE_BUG;
 }
