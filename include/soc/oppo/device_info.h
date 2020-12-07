ea4b4f3232b48546a2c35a676217fb9b2b286807
Irawan's <40514988+dodyirawan85@users.noreply.github.com>
2020-12-07 04:37:36 +0000

    drivers: soc: Import from realme Q-OSS

--- include/soc/oppo/device_info.h
+++ include/soc/oppo/device_info.h
@@ -1,46 +1,43 @@
-/**********************************************************************
-** Copyright 2008-2013 OPPO Mobile Comm Corp., Ltd, All rights reserved.
-** VENDOR_EDIT :                                                                                             
-** File : - SDM660_8.0_LA_2.0\android\kernel\msm-4.4\include\soc\device_info.h
-** ModuleName:devinfo
-** Author : wangjc
-** Version : 1.0
-** Date : 2013-10-23
-** Descriptio : add interface to get device information.
-** History :
-**                  <time>         <author>             <desc>
-**                2013-10-23	    wangjc	            init
-**********************************************************************/
+/**
+ * Copyright 2008-2013 OPPO Mobile Comm Corp., Ltd, All rights reserved.
+ * FileName:devinfo.h
+ * ModuleName:devinfo
+ * Create Date: 2013-10-23
+ * Description:add interface to get device information.
+*/
 
 #ifndef _DEVICE_INFO_H
 #define _DEVICE_INFO_H
 
+#include <linux/list.h>
 
-/*dram type*/
-/*
-enum{
-        DRAM_TYPE0 = 0,
-        DRAM_TYPE1,
-        DRAM_TYPE2,
-        DRAM_TYPE3,
-        DRAM_UNKNOWN,
-};
-*/
-
-enum{
-        MAINBOARD_RESOURCE0 = 0,
-        MAINBOARD_RESOURCE1 = 1,
-        MAINBOARD_RESOURCE2 = 2,
-};
+#define INFO_LEN  24
 
 struct manufacture_info {
-        char *version;
-        char *manufacture;
-        char *fw_path;
+	char name[INFO_LEN];
+	char *version;
+	char *manufacture;
+	char *fw_path;
 };
 
-int register_device_proc(char *name, char *version, char *manufacture);
+struct o_hw_id {
+	const char *label;
+	const char *match;
+	int id;
+	struct list_head list;
+};
+
+int register_device_proc(char *name, char *version, char *vendor);
+/*maxinming_hq added for camera devinfo 20191212*/
+int register_device_proc_cam(char *name, char *version);
 int register_devinfo(char *name, struct manufacture_info *info);
+bool check_id_match(const char *label, const char *id_match, int id);
+
+//#ifdef ODM_WT_EDIT
+//Bo.Zhang@ODM_WT.BSP.TP,2020/04/05, added for TP devinfo begain
+int register_tp_proc(char *name, char *version, char *manufacture ,char *fw_path);
+//Bo.Zhang@ODM_WT.BSP.TP,2020/04/05, added for TP devinfo end
+//#endif /* ODM_WT_EDIT */
 
 
-#endif /*_DEVICE_INFO_H*/
+#endif
