97506c9be81cf2ca1811e4803ba5a4f13b5398e2
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2020-12-10 18:57:22 +0700

    kernel: time: Fix missing merge in 4.14.171

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -102,7 +102,6 @@ static int alarmtimer_rtc_add_device(struct device *dev,
 				struct class_interface *class_intf)
 {
 	unsigned long flags;
-	int err = 0;
 	struct rtc_device *rtc = to_rtc_device(dev);
 	struct wakeup_source *__ws;
 	int ret = 0;
@@ -122,9 +121,9 @@ static int alarmtimer_rtc_add_device(struct device *dev,
 			goto unlock;
 		}
 
-		err = rtc_irq_register(rtc, &alarmtimer_rtc_task);
-		if (err)
-			goto rtc_irq_reg_err;
+		ret = rtc_irq_register(rtc, &alarmtimer_rtc_task);
+		if (ret)
+			goto unlock;
 
 		rtcdev = rtc;
 		/* hold a reference so it doesn't go away */
@@ -136,7 +135,8 @@ unlock:
 	spin_unlock_irqrestore(&rtcdev_lock, flags);
 
 	wakeup_source_unregister(__ws);
-	return err;
+
+	return ret;
 }
 
 static void alarmtimer_rtc_remove_device(struct device *dev,
